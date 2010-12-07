d6ecb925675c70e640ef89009b8e3937fc8f9292
MonsterCoder <MonsterCoder@gmail.com>
2010-12-07 00:14:11 -0600

    registration and settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
-  has_many :feeds
-  has_many :authentications
+  has_many :feeds, :dependent => :delete_all 
+  has_many :authentications, :dependent => :delete_all 
 
   def apply_omniauth(omniauth)
     self.email = omniauth['user_info']['email'] if email.blank?
