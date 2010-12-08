05a8e2c23923056b11ec732793d3813c4aafeba8
MonsterCoder <MonsterCoder@gmail.com>
2010-12-08 00:33:53 -0600

    fixed heroku

--- db/migrate/20101208060706_change_feed_userid_type2.rb
+++ db/migrate/20101208060706_change_feed_userid_type2.rb
@@ -2,6 +2,7 @@ class ChangeFeedUseridType2 < ActiveRecord::Migration
   def self.up
 	remove_column :feeds, :user_id
 	add_column :feeds, :user_id, :integer
+	Feed.reset_column_information 
   end
 
   def self.down
