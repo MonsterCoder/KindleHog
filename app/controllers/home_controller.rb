6523e038f1609ecbb153089cf118205a11aca50c
MonsterCoder <MonsterCoder@gmail.com>
2010-12-24 00:04:22 -0600

    added ignore lisht

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-	redirect_to :controller=>:feeds, :action=>:index if user_signed_in?
+	  redirect_to :controller=>:feeds, :action=>:index if user_signed_in?
   end
 
 end
