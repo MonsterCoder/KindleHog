88242327b46876157e1a5e63eaab99d8eed91b6d
MonsterCoder <MonsterCoder@gmail.com>
2010-12-30 00:21:27 -0600

    complete omniauth authentication

--- spec/controllers/omniauthcontroller_controller_spec.rb
+++ spec/controllers/omniauthcontroller_controller_spec.rb
@@ -2,10 +2,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe OmniauthController do
   include Devise::TestHelpers
-  fixtures :all
-  render_views
- 
-  
+
   it "create action should redirect to root after logging in a user when authentication is found" do
     user = Factory(:user)
     user.authentications.create!(:provider => "twitter", :uid => "123")
@@ -25,11 +22,21 @@ describe OmniauthController do
     response.should redirect_to(root_url)
   end
   
-  it "create action should create a new user with property authentication data" do
-    request.env["omniauth.auth"] = {"provider" => "twitter", "uid" => "456", "user_info"=>{ "email" => "123@gmail.com"}}
+  it "create action should create and signin a new user when first time user sign in with an oauth service" do
+    request.env["omniauth.auth"] = {"provider" => "facebook", "uid" => "456", "user_info"=>{ "email" => "123@gmail.com"}}
     post :create
-    current_user.email.should = "123@gmail.com"
+
+    Authentication.find_by_provider_and_uid("facebook", "456").should_not be_nil
+    Authentication.find_by_provider_and_uid("facebook", "456").user.email.should == "123@gmail.com"
+    @controller.current_user.email.should == "123@gmail.com"
     response.should redirect_to(root_url)
+  end
+  
+  it "create action should redirect to sign-up page when first time user that signs in with an oauth service that does not provide email information" do
+    request.env["omniauth.auth"] = {"provider" => "facebook", "uid" => "456", "user_info"=>{}}
+    post :create
+    session[:omniauth].should_not be_nil
+    response.should redirect_to(new_user_registration_url)
   end
 
 end
